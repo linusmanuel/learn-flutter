fa33a1d6d8822d01c42c20d77f5e9f46105ac681
Joao Lino <linusmannuel@gmail.com>
2025-02-04 07:31:22 +0100

    02: Entendendo container e stack

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.amber,
+            width: 100,
+            height: 100,
+          )
+        ],
+      ),
+    );
+  }
+}
+
