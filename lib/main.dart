ce53b4ca66709cfea7688965de36a234d5ba14d0
Joao Lino <linusmannuel@gmail.com>
2025-02-06 08:14:09 +0100

    02: Utilizando column and Row

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,26 @@ void main() {
   runApp(const MyApp());
 }
 
+Stack _buildStack({
+  Color colorFist = Colors.blue,
+  Color colorSecond = Colors.amber}) {
+  return  Stack(
+    alignment: AlignmentDirectional.center,
+    children: [
+      Container(
+        width: 150,
+        height: 150,
+        color: colorFist,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: colorSecond,
+      ),
+    ],
+  );
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -15,31 +35,20 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.white,
-          ),
-          Container(
-            color: Colors.black,
-            width: 300,
-            height: 300,
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.blue,
-            width: 75,
-            height: 75,
-          ),
-          Container(
-            width: 37,
-            height: 37,
-            color: Colors.amber,
+          _buildStack(colorFist: Colors.red, colorSecond: Colors.white),
+          _buildStack(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.blue, width: 50, height: 50,),
+              Container(color: Colors.red, width: 50, height: 50,),
+              Container(color: Colors.amber, width: 50, height: 50)
+            ],
           )
         ],
       ),
