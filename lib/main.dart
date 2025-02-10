da94bc0025877dc9dbcec619af69f80d51c91495
Joao Lino <linusmannuel@gmail.com>
2025-02-10 09:45:03 +0100

    3: implamentando layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nosso_primeiro_projeto/grid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,13 +18,38 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Text("Flutter: Meus primeiros passos",
-            style: TextStyle(color: Colors.white),),
+          title: Text(
+            "Tarefas",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-        body: Container(),
-        floatingActionButton: FloatingActionButton(onPressed: (){}, backgroundColor: Colors.blue,),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 140,
+              ),
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(color: Colors.black26, width: 72, height: 100,),
+                    Text("Aprender FLutter"),
+                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blue,
+        ),
       ),
     );
   }
 }
-
