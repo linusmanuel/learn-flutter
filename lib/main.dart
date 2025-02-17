db03e1b92bcf0790e4922a1a4b14346f47ab5fd4
Joao Lino <linusmannuel@gmail.com>
2025-02-17 07:46:03 +0100

    1: como serÃa nosso formularia

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/screens/form_screen.dart';
 import 'package:nosso_primeiro_projeto/screens/initial_screen.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: InitialScreen()
+      home: FormScreen()
     );
   }
 }
---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({super.key});
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nova Tarefa', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Container(
+          width: 375,
+          height: 650,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: Colors.black12,
+            border: Border.all(width: 2)
+          ),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
