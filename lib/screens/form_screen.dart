5315b31b5941375bcb11ae7433758f997af1f56c
Joao Lino <linusmannuel@gmail.com>
2025-02-18 11:25:06 +0100

    1: TextField ou TextFormField

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -12,7 +12,10 @@ class _FormScreenState extends State<FormScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nova Tarefa', style: TextStyle(color: Colors.white),),
+        title: Text(
+          'Nova Tarefa',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
       ),
       body: Center(
@@ -20,12 +23,22 @@ class _FormScreenState extends State<FormScreen> {
           width: 375,
           height: 650,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4.0),
-            color: Colors.black12,
-            border: Border.all(width: 2)
-          ),
+              borderRadius: BorderRadius.circular(4.0),
+              color: Colors.black12,
+              border: Border.all(width: 2)),
           child: Column(
-            children: [],
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  textAlign:  TextAlign.center,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(borderSide: BorderSide()),
+                      hintText: 'Nome',
+                      fillColor: Colors.white70, filled: true),
+                ),
+              )
+            ],
           ),
         ),
       ),
