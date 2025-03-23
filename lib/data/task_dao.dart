c48e2c42a04fabbed24e42807016366901372c52
Joao Lino <linusmannuel@gmail.com>
2025-03-23 12:59:42 +0100

    3: aplicando toList()

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -19,8 +19,20 @@ class TaskDao {
     final Database bancoDeDados = await getDatabase();
     final List<Map<String, dynamic>> result = await bancoDeDados.query(_tablename);
     print('Procurando dados no banco de dados... encontrado: $result');
-    return toString();
+    return toList(result);
   }
+
+  List<Task> toList(List<Map<String, dynamic> mapaDeTarefas) {
+    print("Convertendo to List");
+    final List<Task> tarefas = [];
+    for(Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+    print("Lista de tarefas: $tarefas");
+    return tarefas;
+  }
+
   Future<List<Task>> find(String nomeDaTarefa) async {}
   delete(String nomeDaTarefa) async {}
 }
