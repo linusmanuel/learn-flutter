b2aa9583efeb3c0788e94ad5d1266600a13c70c1
Joao Lino <linusmannuel@gmail.com>
2025-03-23 12:37:23 +0100

    3: utilizando o Read

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql = 'CREATE TABLE $_tablename('
@@ -12,7 +14,13 @@ class TaskDao {
   static const String _image = 'image';
 
   save(Task tarefa) async{}
-  Future<List<Task>> findAll() async {}
+  Future<List<Task>> findAll() async {
+    print("Acessando o findAll: ");
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(_tablename);
+    print('Procurando dados no banco de dados... encontrado: $result');
+    return toString();
+  }
   Future<List<Task>> find(String nomeDaTarefa) async {}
   delete(String nomeDaTarefa) async {}
 }
